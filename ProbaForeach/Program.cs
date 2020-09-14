a6f65ace5472650eb7525aa0ceee19400f849656
SzklevanMilan <szklevanm@gmail.com>
2020-09-14 13:54:09 +0200

    Foreach

--- ProbaForeach/Program.cs
+++ ProbaForeach/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ProbaForeach
 {
@@ -10,6 +11,15 @@ namespace ProbaForeach
     {
         static void Main(string[] args)
         {
+            int[] tomb = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int szum = 0;
+            foreach (var t in tomb)
+            {
+                szum += t;
+                Console.WriteLine(t.ToString());
+            }
+            Console.WriteLine("Az összegük: {0}.",szum);
+            Console.ReadKey();
         }
     }
 }
